b423b0745d6ceaf00f977ef5bb52e4c1cece6482
Waqas Ahmed <vvaqasahmed27@gmail.com>
2025-08-28 18:49:52 +0500

    Added functionality for daily challenges

---
+++ Dailychallenge.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Apex_Quiz
+{
+    partial class DailyChallengeForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(29, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(372, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "option1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.OptionButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(431, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(350, 60);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "option2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.OptionButton_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Blue;
+            this.button3.Location = new System.Drawing.Point(29, 331);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(372, 59);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "option3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.OptionButton_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Yellow;
+            this.button4.Location = new System.Drawing.Point(431, 331);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(350, 59);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "option4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.OptionButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold);
+            this.textBox1.Location = new System.Drawing.Point(227, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(310, 25);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "          DAILY CHALLENGE";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(390, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // DailyChallengeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(807, 443);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "DailyChallengeForm";
+            this.Text = "Daily Challenge";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Dailychallenge.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Apex_Quiz
+{
+    public partial class DailyChallengeForm : Form
+    {
+        private string correctAnswer;
+        private int dailyStreak = 0;
+        private static string cooldownPath = "daily_cooldown.txt";
+
+        public DailyChallengeForm()
+        {
+            if (IsCooldownActive())
+            {
+                MessageBox.Show("Please wait! Daily Challenge will be available after 24 hours.");
+                return;
+            }
+
+            InitializeComponent();
+            LoadDailyChallengeQuestion();
+        }
+
+        private void LoadDailyChallengeQuestion()
+        {
+            using (MySqlConnection conn = DatabaseHelper.GetConnection())
+            {
+                conn.Open();
+                string query = @"
+                    SELECT question_text, option_a, option_b, option_c, option_d, correct_option, q.quiz_id, t.category 
+                    FROM questions q
+                    JOIN quizzes t ON q.quiz_id = t.quiz_id
+                    ORDER BY RAND()
+                    LIMIT 1";
+
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        label1.Text = reader["question_text"].ToString();
+                        textBox1.Text = "Category: " + reader["category"].ToString();
+
+                        button1.Text = reader["option_a"].ToString();
+                        button2.Text = reader["option_b"].ToString();
+                        button3.Text = reader["option_c"].ToString();
+                        button4.Text = reader["option_d"].ToString();
+
+                        correctAnswer = reader["correct_option"].ToString();
+                    }
+                }
+            }
+        }
+
+        private void OptionButton_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            if (btn.Text == correctAnswer)
+            {
+                dailyStreak++;
+                MessageBox.Show($"Correct! Streak: {dailyStreak}");
+            }
+            else
+            {
+                dailyStreak = 0;
+                MessageBox.Show("Incorrect! Streak reset.");
+            }
+
+            SaveCooldown();
+            this.Close();
+        }
+
+        private void SaveCooldown()
+        {
+            File.WriteAllText(cooldownPath, DateTime.Now.ToString());
+        }
+
+        private bool IsCooldownActive()
+        {
+            if (!File.Exists(cooldownPath))
+                return false;
+
+            DateTime lastPlayed = DateTime.Parse(File.ReadAllText(cooldownPath));
+            return (DateTime.Now - lastPlayed).TotalHours < 24;
+        }
+
+        // Empty handlers to avoid compiler error
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        private void Form1_Load(object sender, EventArgs e) { }
+    }
+}
